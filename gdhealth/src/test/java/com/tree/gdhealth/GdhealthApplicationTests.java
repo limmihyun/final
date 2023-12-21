73c4f6befe6418fcc2ef7764df1cd25ddae4ed22
limmihyun <myhun1218@gmail.com>
2023-12-21 10:50:20 +0900

    최초

---
+++ gdhealth/src/test/java/com/tree/gdhealth/GdhealthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tree.gdhealth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GdhealthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
