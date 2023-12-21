97050b4de5755e0ec710d34a38c1b49a3f8a5484
MASKUN2 <maskunflower2@naver.com>
2023-12-21 16:56:04 +0900

    [정인호] datasource 연결확인용 테스트코드 작성

--- src/test/java/com/tree/gdhealth/GdhealthApplicationTests.java
+++ src/test/java/com/tree/gdhealth/GdhealthApplicationTests.java
@@ -1,13 +1,47 @@
 package com.tree.gdhealth;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import javax.sql.*;
+
+import java.sql.*;
+import static org.assertj.core.api.Assertions.*;
+
+/**
+ * @author  정인호 <br>
+ * 데이터베이스 연결 확인용 테스트
+ */
+
+@Slf4j
 @SpringBootTest
 class GdhealthApplicationTests {
 
+	@Autowired
+	DataSource dataSource;
+
 	@Test
-	void contextLoads() {
+	void 데이터베이스연결을확인하고_데이터를가져온다() throws SQLException {
+		//given
+		Connection conn = dataSource.getConnection();
+		PreparedStatement ps = conn.prepareStatement("select testContent from test");
+		String assertStr = null;
+
+		//when
+		ResultSet rs = ps.executeQuery();
+
+		//then
+		if(rs.next()){
+			assertStr = rs.getString(1);
+		}
+		rs.close();
+		ps.close();
+		conn.close();
+
+		log.info("assertStr = {}", assertStr);
+		assertThat(assertStr).isNotNull();
 	}
 
 }
