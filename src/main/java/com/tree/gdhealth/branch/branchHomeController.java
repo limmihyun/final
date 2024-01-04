4dbe1cefdb9c3029f018a1e11f78779f8706e1f4
MASKUN2 <maskunflower2@naver.com>
2024-01-04 12:26:01 +0900

    [정인호] fix: db 컬럼수정에 대한 영향을 지점 도메인에 반영 (#90)
    T1222
    - 오전 db 컬럼수정에 대한 영향을 지점 도메인에 반영함
    - 빠른 직원로그인을 위해 지점페이지에 테스트로그인버튼 생성

--- src/main/java/com/tree/gdhealth/branch/branchHomeController.java
+++ src/main/java/com/tree/gdhealth/branch/branchHomeController.java
@@ -1,5 +1,10 @@
 package com.tree.gdhealth.branch;
 
+import com.tree.gdhealth.employee.login.EmpLoginService;
+import com.tree.gdhealth.employee.login.LoginEmployee;
+import com.tree.gdhealth.vo.Employee;
+import jakarta.servlet.http.HttpSession;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,11 +12,23 @@ import org.springframework.web.bind.annotation.GetMapping;
 /**
  * @author 정인호
  */
+@RequiredArgsConstructor
 @Controller
 public class branchHomeController {
+    private final EmpLoginService empLoginService;
     @GetMapping("/branch/home")
     public String getHome(){
         log.debug("/branch/home");
+        return "/branch/home";
+    }
+
+    @GetMapping("/testLoginEmployee")
+    public String testLogin(HttpSession session){
+        Employee employee = new Employee();
+        employee.setEmployeeId("gasan1manager");
+        employee.setEmployeePw("1234");
+        LoginEmployee loginEmployee = empLoginService.login(employee);
+        session.setAttribute("loginEmployee", loginEmployee);
         return "/branch/home";
     }
 }
