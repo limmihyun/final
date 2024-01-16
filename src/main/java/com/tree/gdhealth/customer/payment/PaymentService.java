c92dd36f6a8d3790352a9f70a5a8401c12edc214
wkdgustn097 <wkdgustn097@gmail.com>
2024-01-16 18:10:16 +0900

    [장현수] toss widget을 사용한 결제 페이지 구현

--- src/main/java/com/tree/gdhealth/customer/payment/PaymentService.java
+++ src/main/java/com/tree/gdhealth/customer/payment/PaymentService.java
@@ -1,5 +1,27 @@
 package com.tree.gdhealth.customer.payment;
 
-public class PaymentService {
+import java.util.HashMap;
+import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+public class PaymentService {
+	
+	@Autowired
+	private PaymentMapper paymentMapper;
+	
+	public int addPayment(int membershipNo, int customerNo) {
+		
+		Map<String, Object> paramap = new HashMap<>();
+		paramap.put("membershipNo", membershipNo);
+		paramap.put("customerNo", customerNo);
+		
+		paymentMapper.addPayment(paramap);
+
+		return 1;
+	}
 }
